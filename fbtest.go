b4d7b9ff74e8448b9c1b3b14d3a20442d7d1c97b
mamu <mamu@stablerock.de>
2017-01-18 12:04:53 +0100

    zu anna fahren

--- fbtest.go
+++ fbtest.go
@@ -4,20 +4,63 @@ import (
 	"github.com/kaey/framebuffer"
 	"log"
 	"fmt"
+	"net"
+	"bufio"
+	"regexp"
 )
 
+var xsize, ysize int
+
+type pixel struct {
+	x, y, a, r, g, b int
+}
+
+
 func main() {
+	c := make(chan pixel)
+	cs := (chan<- pixel) (c)
+	cr := (<-chan pixel) (c)
+	go fb_mgmt(cr)
+	//c <- pixel{i, j, 0, 0, 255, 0}
+	ln, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go handleConnection(conn, cs)
+	}
+}
+
+func fb_mgmt(c <-chan pixel) {
 	fb, err := framebuffer.Init("/dev/fb0")
 	if err != nil {
 		log.Fatal(err)
 	}
-	x, y := fb.Size()
-	fmt.Printf("Screensize: %dx%d\n", x, y)
-	// red, green, blue, alpha
-	for i := 100; i < 200; i++ {
-		for j := 100; j < 200; j++ {
-			fb.WritePixel(i, j, 0x00, 0x00, 0xFF, 0x00)
+	xsize, ysize := fb.Size()
+	fmt.Println(xsize, ysize)
+	for {
+		px := <-c
+		fb.WritePixel(px.x, px.y, px.r, px.g, px.b, px.a)
+	}
+}
+
+func handleConnection(conn net.Conn, c chan<- pixel) {
+	var err error
+	var status string
+	fmt.Println("Neue Connection: ", conn.RemoteAddr().String())
+	for err == nil {
+		status, err = bufio.NewReader(conn).ReadString('\n')
+		fmt.Print("received: ", status[:len(status) - 1])
+		matched, _ := regexp.Match("insert regex here", []byte(status))
+		if matched {
+			fmt.Println(" good")
+		} else {
+			fmt.Println(" bad")
 		}
 	}
-	fb.Close()
+	fmt.Print("Connection closed.")
 }
